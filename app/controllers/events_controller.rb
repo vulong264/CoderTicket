7c8faa226a9671c25856a0c1d6bc96bc87886d3b
Vu Long <vulong264@gmail.com>
2016-11-02 21:41:42 +0700

    completed all required userstory

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -78,7 +78,26 @@ class EventsController < ApplicationController
 	end
 
 	def update
-		raise 'update'
+		#create venue
+		@venue = Venue.find_or_create_by(name: params[:event][:venue_name],
+		full_address: params[:event][:venue_address],
+		region_id: params[:event][:region_id])
+		#create event
+		@event.starts_at = Date.parse(get_starts_at(params[:event]).to_s)
+		@event.ends_at = Date.parse(get_ends_at(params[:event]).to_s)
+		@event.venue_id = @venue.id
+		@event.hero_image_url = params[:event][:hero_image_url]
+		@event.extended_html_description = params[:event][:extended_html_description]
+		@event.category_id = params[:event][:category_id]
+		@event.name = params[:event][:name]
+		@event.user_id = current_user.id
+		if @event.save
+			flash[:success] = "Event updated successfully!"
+			redirect_to event_path(@event.id)
+		else
+			flash[:error] = @event.errors.full_messages.to_sentence
+			redirect_to 'edit'
+		end
 	end
 
 	def add_ticket
